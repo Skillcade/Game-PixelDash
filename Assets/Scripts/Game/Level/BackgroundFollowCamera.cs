c2eb278e2683a5ea7b1e82516b61ad6f8fbad1c4
Aleksandr Litskevich <avlitskevich@gmail.com>
2026-01-11 13:18:26 +0500

    Started recreating level with new sprites

---
+++ Assets/Scripts/Game/Level/BackgroundFollowCamera.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+namespace Game.Level
+{
+    public class BackgroundFollowCamera : MonoBehaviour
+    {
+        [SerializeField] private Transform _cameraTransform;
+        [SerializeField] private Vector2 _cameraBounds;
+        [SerializeField] private Vector2 _selfBounds;
+
+        private void Update()
+        {
+            var cameraLerpValue = Mathf.InverseLerp(_cameraBounds.x, _cameraBounds.y, _cameraTransform.position.x);
+            var selfPositionX = Mathf.Lerp(_selfBounds.x, _selfBounds.y, cameraLerpValue);
+            transform.position = new Vector3(selfPositionX, transform.position.y, transform.position.z);
+        }
+    }
+}
