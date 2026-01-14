2295a62166f8158e432c911a50a82788cea26ec3
Aleksandr Litskevich <avlitskevich@gmail.com>
2026-01-14 23:07:13 +0500

    Updated level and enemies

---
+++ Assets/Scripts/Game/Level/ObjectRotator.cs
@@ -0,0 +1,18 @@
+﻿using FishNet.Object;
+using UnityEngine;
+
+namespace Game.Level
+{
+    public class ObjectRotator : NetworkBehaviour
+    {
+        [SerializeField] private float _rotationSpeed;
+
+        private void Update()
+        {
+            if (!IsServerInitialized)
+                return;
+            
+            transform.rotation *= Quaternion.Euler(0f, 0f, _rotationSpeed * Time.deltaTime);
+        }
+    }
+}
